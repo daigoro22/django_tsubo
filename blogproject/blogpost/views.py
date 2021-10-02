6a2d8ad8514b491d0c87484bc68b17ed01676ded
daigoro22 <d183303@edu.tut.ac.jp>
2021-10-02 14:53:49 +0900

    feat: chapter4 完了

--- blogproject/blogpost/views.py
+++ blogproject/blogpost/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 from django.views.generic import(
     ListView,
     DetailView,
+    CreateView,
+    DeleteView,
+    UpdateView,
 )
 from .models import BlogModel
+from django.urls import reverse_lazy
 
 # Create your views here.
 class BlogList(ListView):
@@ -14,3 +18,20 @@ class BlogList(ListView):
 class BlogDetail(DetailView):
     template_name = 'detail.html'
     model = BlogModel
+
+class BlogCreate(CreateView):
+    template_name = 'create.html'
+    model = BlogModel
+    fields = ('title','category','content')# フォームで入力させるフィールドを指定
+    success_url = reverse_lazy('list')
+
+class BlogDelete(DeleteView):
+    template_name = 'delete.html'
+    model = BlogModel
+    success_url = reverse_lazy('list')
+
+class BlogUpdate(UpdateView):
+    template_name = 'update.html'
+    model = BlogModel
+    success_url = reverse_lazy('list')
+    fields = ('title','category','content')
