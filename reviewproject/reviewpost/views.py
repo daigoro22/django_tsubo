88162cc0be9f25ddc33bf587e4da0cd639f62797
daigoro22 <d183303@edu.tut.ac.jp>
2021-10-03 14:03:18 +0900

    feat: ユーザ登録機能追加

--- reviewproject/reviewpost/views.py
+++ reviewproject/reviewpost/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
 def signupview(request):
     if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        user = User.objects.create_user(username_data,'',password_data)
         print('POST method')
     else:
+        print(User.objects.all())
         print('Get method probably...')
     return render(request, 'signup.html',{})
