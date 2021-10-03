105c52f46ceb5f9efec8a251e8cc7224163c0115
daigoro22 <d183303@edu.tut.ac.jp>
2021-10-03 14:08:23 +0900

    feat: ユーザ名重複のエラー表示実装

--- reviewproject/reviewpost/views.py
+++ reviewproject/reviewpost/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.db import IntegrityError
 
 # Create your views here.
 def signupview(request):
     if request.method == 'POST':
         username_data = request.POST['username_data']
         password_data = request.POST['password_data']
-        user = User.objects.create_user(username_data,'',password_data)
+        try:
+            user = User.objects.create_user(username_data,'',password_data)
+        except IntegrityError:
+            return render(request,'signup.html',{'error':'このユーザはすでに登録されています'})
         print('POST method')
     else:
         print(User.objects.all())
