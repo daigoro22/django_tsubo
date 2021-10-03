4f579a4d0ad85267d012d853bf7ded42d84c0fa2
daigoro22 <d183303@edu.tut.ac.jp>
2021-10-03 13:44:29 +0900

    feat: サインアップビューを作成

--- reviewproject/reviewpost/views.py
+++ reviewproject/reviewpost/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def signupview(request):
+    if request.method == 'POST':
+        print('POST method')
+    else:
+        print('Get method probably...')
+    return render(request, 'signup.html',{})
