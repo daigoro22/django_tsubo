9a436e49ab07ba7be9cb855094e342c4ef91d5af
daigoro22 <d183303@edu.tut.ac.jp>
2021-10-08 09:11:17 +0900

    feat: detail view の作成

--- reviewproject/reviewpost/urls.py
+++ reviewproject/reviewpost/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from .views import (
     signupview,
     loginview,
-    listview
+    listview,
+    detailview
 )
 
 urlpatterns = [
     path('signup/',signupview,name='signup'),
     path('login/',loginview,name='login'),
     path('list/',listview,name='list'),
+    path('detail/<int:pk>',detailview,name='detail'),
 ]
