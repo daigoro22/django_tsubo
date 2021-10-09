f3ef9669f8fbb8fd4ca509acbb5817597a897a49
daigoro22 <d183303@edu.tut.ac.jp>
2021-10-09 17:40:27 +0900

    feat: Django 標準機能でパスワード変更機能作成

--- passwordproject/passwordproject/urls.py
+++ passwordproject/passwordproject/urls.py
@@ -23,4 +23,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('django.contrib.auth.urls')),
+    path('change-password/',
+        auth_views.PasswordChangeView.as_view(template_name='change-password.html'))
 ]
